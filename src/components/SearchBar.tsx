85acb669824e4100213fe3e7085620aa1e6276ef
Roy Arnold <rfawebdesigns@gmail.com>
2026-08-31 11:29:38 +0100

    Add search input validation

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -7,11 +7,17 @@ type SearchBarProps = {
   };
 
 function SearchBar({ setCity }: SearchBarProps) {
+    const handleSearch = () => {
+        const trimmedSearch = searchTerm.trim();
+        if (trimmedSearch) {
+          setCity(trimmedSearch);
+        }
+      };
     const [searchTerm, setSearchTerm] = useState("");
     return (
         <div className="search-bar">
             <input onChange={(event) => setSearchTerm(event.target.value)} type="text" placeholder="Search for a city..." />
-            <button onClick={() => setCity(searchTerm)}>Search</button>       
+            <button onClick={handleSearch}>Search</button>       
             </div>
     )
   }
